8a910f438d8202b417ddaf1f18499bec98fdb77c
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-02-28 13:54:14 +0300

    lesson 01

---
+++ SmartEdu/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Index Sayfasi');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
