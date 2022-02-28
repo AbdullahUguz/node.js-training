3101a977e8ab1a5ec0da1e7cbcd3fc6d12954cf8
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-02-28 14:36:13 +0300

    lesson 02

--- SmartEdu/app.js
+++ SmartEdu/app.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const app = express();
 
+
+// Template 
+app.set("view engine","ejs");
+
+//Middlewears
+app.use(express.static("public"));
+
+//Routes
+
 app.get('/', (req, res) => {
-  res.send('Index Sayfasi');
+  res.status(200).render('index',{
+    page_name:"index"
+  });
 });
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about',{
+    page_name:"about"
+  });
+});
+
 
 const port = 3000;
 
