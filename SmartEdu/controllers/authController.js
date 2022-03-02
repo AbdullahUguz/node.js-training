96e1cedb48b40a8d1e32a8d34a49eb1802f47ba4
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-03-03 01:38:55 +0300

    dashboard page created

--- SmartEdu/controllers/authController.js
+++ SmartEdu/controllers/authController.js
@@ -28,7 +28,7 @@ exports.loginUser = async (req, res) => {
         if (same) {
           // user session
           req.session.userID = user._id;
-          res.status(200).redirect('/');
+          res.status(200).redirect('/users/dashboard');
           //  res.status(200).send('You are logged in');
         }
       });
@@ -44,5 +44,14 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = async (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+
+  const user = await User.findOne({_id:req.session.userID})
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
   });
 };
