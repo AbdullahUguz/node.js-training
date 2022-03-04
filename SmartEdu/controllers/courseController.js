d9f99a7740a8c8ff0cc2282c88646207905222e5
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-03-05 00:25:18 +0300

    student teacher relation created

--- SmartEdu/controllers/courseController.js
+++ SmartEdu/controllers/courseController.js
@@ -41,13 +41,15 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
+
+    const user = await User.findById(req.session.userID);
     const course = await Course.findOne({ slug: req.params.slug }).populate('user');
   //  const user = await User.findById(course.user);
     const categories = await Category.find();
     res.status(200).render('course', {
       course,
       categories,
-    //  user,
+      user,
       page_name: 'courses',
     });
   } catch (error) {
@@ -63,6 +65,24 @@ exports.enrollCourse = async (req, res) => {
     
     const user =await User.findById(req.session.userID);
     await user.courses.push({_id:req.body.course_id});
+    await user.save();
+
+
+    res.status(200).redirect('/users/dashboard')
+
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.releaseCourse = async (req, res) => {
+  try {
+    
+    const user =await User.findById(req.session.userID);
+    await user.courses.pull({_id:req.body.course_id});
     await user.save();
 
 
--- SmartEdu/routes/courseRoute.js
+++ SmartEdu/routes/courseRoute.js
@@ -14,4 +14,6 @@ router.route('/:slug').get(courseController.getCourse);
 
 router.route('/enroll').post(courseController.enrollCourse);
 
+router.route('/release').post(courseController.releaseCourse);
+
 module.exports = router;
