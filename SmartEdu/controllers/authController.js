47dd9f2b44ecdb228bbed055830fa9f83ee92352
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-03-07 20:02:01 +0300

    user control added

--- SmartEdu/controllers/authController.js
+++ SmartEdu/controllers/authController.js
@@ -6,15 +6,20 @@ const Course = require('../models/Course');
 
 const bcrypt = require('bcrypt');
 
+const { validationResult } = require('express-validator');
+
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult(req);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash('error', `${errors.array()[i].msg}`);
+    }
+
+    res.status(201).redirect('/register');
   }
 };
 
@@ -30,9 +35,15 @@ exports.loginUser = async (req, res) => {
           // user session
           req.session.userID = user._id;
           res.status(200).redirect('/users/dashboard');
-          //  res.status(200).send('You are logged in');
+          // res.status(200).send('You are logged in');
+        } else {
+          req.flash('error', 'Your password is not correct!');
+          res.status(400).redirect('/login');
         }
       });
+    } else {
+      req.flash('error', 'User is not exist!');
+      res.status(400).redirect('/login');
     }
   } catch (error) {
     res.status(400).json({
@@ -49,15 +60,16 @@ exports.logoutUser = async (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-
-  const user = await User.findOne({_id:req.session.userID}).populate('courses');
-  const categories =await Category.find();
-  const courses = await Course.find({user:req.session.userID});
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    'courses'
+  );
+  const categories = await Category.find();
+  const courses = await Course.find({ user: req.session.userID });
 
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
     categories,
-    courses
-  }); 
+    courses,
+  });
 };
